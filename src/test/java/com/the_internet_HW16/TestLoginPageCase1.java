bfbbb1903b7f7b22c6cef0c7ad95d2f355109e54
VladOstapchuk <vlad.ostapchuk@gmail.com>
2024-07-12 01:05:58 +0300

    Selenium HW16 12072024

---
+++ src/test/java/com/the_internet_HW16/TestLoginPageCase1.java
@@ -0,0 +1,64 @@
+package com.the_internet_HW16;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class TestLoginPageCase1 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeClass
+    public void startdriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void testLoginPageCase1() {
+        //відкрити сторінку з цією URL
+        driver.get("https://the-internet.herokuapp.com/login");
+        //перевірка, що сторінка загрузилась
+        WebElement titlepage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='Login Page']")));
+        //перевірка що на сторінці є текст
+        Assert.assertEquals(titlepage.getText(), "Login Page", "Page Title is not correct");
+        //заповнюю логін, пароль та натисаю кноку Логін
+        WebElement usernamefield = driver.findElement(By.xpath("//input[@type='text' and @name='username']"));
+        WebElement passwordfield = driver.findElement(By.xpath("//input[@type='password' and @name='password']"));
+        usernamefield.sendKeys("tomsmith" );
+        passwordfield.sendKeys("SuperSecretPassword!");
+        WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit']/i"));
+        submitButton.click();
+        //перевірка URL cторінки, що завантажилась
+        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/secure", "URL is not correct!");
+        //перевірка наявності флешки з текстом
+        WebElement flash = driver.findElement(By.xpath("//div[@id='flash']"));
+        Assert.assertTrue(flash.getText().contains("You logged into a secure area!"), "Text is not correct!");
+        //Натиснути на кнопку Logout
+        WebElement logoutButton = driver.findElement(By.xpath("//a[@href='/logout']/i[text()=' Logout']"));
+        logoutButton.click();
+        //перевірка, що завантажилась сторінка з потрібним URL
+        Assert.assertEquals(driver.getCurrentUrl(), "https://the-internet.herokuapp.com/login", "URL is not correct!");
+        //перевірка що на сторінці є флеша з потрібним текстом
+        WebElement flashloggedout = driver.findElement(By.xpath("//div[@id='flash' and @class='flash success']"));
+        Assert.assertTrue(flashloggedout.getText().contains("You logged out of the secure area!"), "Text is not correct!");
+    }
+
+    @AfterClass
+    public void closeDive(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/the_internet_HW16/TestLoginPageCase3.java
@@ -0,0 +1,52 @@
+package com.the_internet_HW16;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class TestLoginPageCase3 {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeClass
+    public void startdriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void testLoginPageCase3() {
+        //відкрити сторінку з цією URL
+        driver.get("https://the-internet.herokuapp.com/login");
+        //перевірка, що сторінка загрузилась
+        WebElement titlepage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[text()='Login Page']")));
+        //перевірка що на сторінці є текст
+        Assert.assertEquals(titlepage.getText(), "Login Page", "Page Title is not correct");
+        //заповнюю логін, пароль та натисаю кноку Логін
+        WebElement passwordfield = driver.findElement(By.xpath("//input[@type='password' and @name='password']"));
+        passwordfield.sendKeys("SuperSecretPassword!");
+        WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit']/i"));
+        submitButton.click();
+        //перевірка наявності флешки з текстом
+        WebElement redflash = driver.findElement(By.xpath("//div[@id='flash' and @class='flash error']"));
+        Assert.assertTrue(redflash.getText().contains("Your username is invalid!"), "Text is not correct!");
+    }
+
+    @AfterClass
+    public void closeDive(){
+        driver.quit();
+    }
+}
